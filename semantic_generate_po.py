3c5cae69b1a34bc5e0f8136c7a4eb50915535dff
Drupchen <hhdrupchen@gmail.com>
2020-09-03 17:07:38 +0100

    update

--- semantic_generate_po.py
+++ semantic_generate_po.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 import re
-from botok import Text
+from uuid import uuid4
 import polib
+from antx import transfer
+from botok import Text
 
 
 class Po:
     def __init__(self):
+        self.transfer = Transfer()
         self.file = polib.POFile()
         self.file.metadata = {
             'MIME-Version': '1.0',
@@ -34,8 +37,10 @@ class Po:
     def write_to_file(self, filename):
         self.file.save(filename)
 
-    def lines_to_entries(self, lines, origin):
-        for num, line in enumerate(lines):
+    def lines_to_entries(self, dump, po_file):
+        lines = self.transfer.generate_entries(dump, po_file)
+        for num, l in enumerate(lines):
+            line, ctxt = l
             no_notes = self.remove_peydurma_notes(line)
             if no_notes == "":
                 no_notes, line = line, no_notes
@@ -46,13 +51,13 @@ class Po:
             # format tokens
             no_notes = re.sub('([^།་_]) ([^_།་])', '\g<1>␣\g<2>', no_notes)   # affixed particles
             no_notes = re.sub('_', ' ', no_notes)   # spaces
-            self._create_entry(msgid=no_notes, msgctxt=f'line {num+1}, {origin}', tcomment=line)
+            self._create_entry(msgid=no_notes, msgctxt=ctxt, tcomment=line)
 
     def txt_to_po(self, filename):
-        lines = filename.read_text(encoding='utf-8').strip().split('\n')
-        self.lines_to_entries(lines, filename.name)
-
+        lines = filename.read_text(encoding='utf-8')
         outfile = filename.parent / (filename.stem + ".po")
+
+        self.lines_to_entries(lines, outfile)
         self.write_to_file(outfile)
 
     @staticmethod
@@ -70,6 +75,51 @@ class Po:
             return ''.join([a for a in note if not a.startswith('<')]).replace(':', '')
         else:
             return ""
+
+
+class Transfer:
+    """
+    limitation : in case a line is split on two lines in the updated .txt, it will keep
+    the same uuid on the second line and only add a new uuid on the first line.
+    """
+    def __init__(self):
+        self.transfer = transfer
+
+    def generate_entries(self, dump, po_file):
+        if po_file.is_file():
+            dump = self.extract_entries(dump, po_file)
+        updated = self.add_missing_uuids(dump)
+        entries = []
+        for line in updated.strip().split('\n'):
+            line = line.strip()
+            txt, ctxt = line[:-1].split('—')
+            entries.append([txt, ctxt])
+        return entries
+
+    def extract_entries(self, dump, po_file):
+        po_file = polib.pofile(po_file)
+        po_entries = []
+        for p in po_file:
+            if p.tcomment:
+                line = p.tcomment
+            else:
+                line = p.msgid.replace(' ', '').replace('–', ' ')
+            po_entries.append([line, p.msgctxt])
+        po_dump = '\n'.join([''.join((a, f'—{b}—')) for a, b in po_entries])
+        pattern = [['uuid', '(—.+?—)']]
+        transfered = self.transfer(po_dump, pattern, dump, 'txt')
+        return transfered
+
+    def add_missing_uuids(self, dump):
+        lines = dump.strip().split('\n')
+        for num, l in enumerate(lines):
+            l = l.strip()
+            if not l.endswith('—'):
+                lines[num] = l + f'—{self.get_unique_id()}—'
+        return '\n'.join(lines)
+
+    def get_unique_id(self):
+        return uuid4().hex
 
 
 if __name__ == '__main__':
